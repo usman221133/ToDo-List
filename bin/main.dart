7db133d1df2e5f7f4a9c3cc0fc97421ea349d6f4
usman221133 <majorusmanafzal786@gmail.com>
2020-02-06 18:42:07 +0500

    Completed

--- bin/main.dart
+++ bin/main.dart
@@ -7,15 +7,14 @@ void main() {
   var input = stdin.readLineSync();
   var optionNumber = int.tryParse(input) ?? -1;
 
-  do{
-    switch(optionNumber)
-    {
+  do {
+    switch (optionNumber) {
       case 1:
         printList(toDoList);
 
         optionList(toDoList);
 
-        print(Process.runSync("clear", [], runInShell: true).stdout);
+//        print(Process.runSync("cls", [], runInShell: true).stdout);
 
         mainMenu();
         break;
@@ -37,13 +36,12 @@ void main() {
         break;
     }
 
-    input= stdin.readLineSync();
+    input = stdin.readLineSync();
     optionNumber = int.tryParse(input) ?? -1;
-  }
-  while(optionNumber != 4);
+  } while (optionNumber != 4);
 }
 
-void mainMenu(){
+void mainMenu() {
   print('======== TODO LIST =========');
   print('Press 1 for TODO LIST');
   print('Press 2 to ADD TODOS');
@@ -51,14 +49,13 @@ void mainMenu(){
   print('Press 4 to exit\n');
 }
 
-void printList(List<String> toDoList){
+void printList(List<String> toDoList) {
   print('======== LIST =========');
-  if(toDoList.length == 0){
+  if (toDoList.length == 0) {
     print('\t Empty');
-  }
-  else{
-    for(var i=0;i<toDoList.length; i++){
-      print((i+1).toString()+': '+toDoList[i]);
+  } else {
+    for (var i = 0; i < toDoList.length; i++) {
+      print((i + 1).toString() + ': ' + toDoList[i]);
     }
   }
   print('======== END OF LIST =========');
@@ -67,10 +64,11 @@ void printList(List<String> toDoList){
   print('Press 3 to return to Main Menu');
 }
 
-void optionList(List<String> toDoList){
-  var listOption = int.tryParse(stdin.readLineSync()) ?? -1;
-  do{
-    switch(listOption){
+void optionList(List<String> toDoList) {
+  var listOption = 0;
+  do {
+    listOption = int.tryParse(stdin.readLineSync()) ;
+    switch (listOption) {
       case 1:
         print('Press id from list to Edit');
         editElement(toDoList);
@@ -80,59 +78,51 @@ void optionList(List<String> toDoList){
         deleteElement(toDoList);
         break;
       case 3:
+        return;
+      default:
+        print('Invalid Value');
         break;
-        default:
-          print ('Invalid Value');
-          break;
     }
-    listOption = int.tryParse(stdin.readLineSync()) ?? -1;
-  }while(listOption != 3);
+  } while (listOption != 3);
 }
 
 void deleteElement(List<String> toDoList) {
-  var deleteId = int.tryParse(stdin.readLineSync()) ?? -1;
+  var deleteId = int.tryParse(stdin.readLineSync());
   if (deleteId == -1) {
     print('Invalid Input for ID');
-  }
-  else {
-       deleteId-=1;
+  } else {
+    deleteId -= 1;
     if (deleteId < toDoList.length) {
       var name = toDoList[deleteId];
 
       toDoList.removeAt(deleteId);
-      print(deleteId.toString() + ': ' + name +' has been removed from the list.');
-    }
-    else {
+      print(deleteId.toString() +
+          ': ' +
+          name +
+          ' has been removed from the list.');
+    } else {
       print("Provided Id missing or don't exist in List");
     }
   }
 }
 
-void editElement(List<String> toDOlist){
-
+void editElement(List<String> toDOlist) {
   var editID = int.tryParse(stdin.readLineSync()) ?? -1;
-    if ( editID == -1) {
+  if (editID == -1) {
     print('Invalid Input For ID');
-  }
-
-    else{
-      editID-=1;
-      if (editID < toDOlist.length){
-        var newName = stdin.readLineSync();
-        var oldName  = toDOlist[editID];
-        var Update = toDOlist[editID];
-        toDOlist[editID] = newName;
+  } else {
+    editID -= 1;
+    if (editID < toDOlist.length) {
+      var newName = stdin.readLineSync();
+      var oldName = toDOlist[editID];
+      var Update = toDOlist[editID];
+      toDOlist[editID] = newName;
 //        var newInput= stdin.readLineSync();
-        newName = (newName) ?? -1;
+      newName = (newName) ?? -1;
 
-        print(oldName+ ' has been changed to ' +Update+ '.');
-      }
-      else{
-        print("Provided Id missing or don't exist in List");
-      }
+      print(oldName + ' has been changed' '.');
+    } else {
+      print("Provided Id missing or don't exist in List");
     }
-
-
-
-
+  }
 }
